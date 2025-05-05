951b89e241e772dc3f4893cb160f06149f669c69
Kadnan <ayeshoali@gmail.com>
2025-05-05 17:36:28 +0500

    feat: imlemented navicons logic

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,18 +17,26 @@ const Navbar = () => {
       {/* Not Mobile View */}
       <div className="hidden md:flex items-center justify-between h-full gap-8">
         {/* Left Side of navbar*/}
-        <div className="w-1/3">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
         <Link href="/" className="flex items-center gap-3">
           <Image src='/logo.png' alt="Logo" width={24} height={24}/>
           <div className="text-2xl tracking-wide">Meiraj</div>
         </Link>
         </div>
+
+        <div className=" hidden xl:flex  gap-4">
+          <Link href='/'>HomePage </Link>
+          <Link href='/'>Shop </Link>
+          <Link href='/'>Deals </Link>
+          <Link href='/'>About </Link>
+          <Link href='/'>Contact </Link>
+        </div>
         
        {/* Right Side of navbar */}
-       <div className="w-2/3 fles items-center justify-between gap-8">
-       <SearchBar/>
-       <NavIcons/>
-       {/* <Menu/> */}
+       <div className="w-2/3 flex items-center justify-between gap-8 xl:w-1/2">
+        <SearchBar/>
+        <NavIcons/>
+        {/* <Menu/> */}
        </div>
          
          </div>
--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,13 +1,32 @@
 "use client"
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SearchBar = () => {
+
+const router = useRouter();
+
+const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const searchText = formData.get("search-text") as string;
+
+    if(searchText){
+      router.push(`/list?name=${searchText}`)
+    }
+}
+
   return ( 
-      <form className="flex ic justify-between items-center gap-4 bg-gray-100 p-2 rounded-md flex-1">
+      <form 
+      className="flex ic justify-between items-center gap-4 bg-gray-100 p-2 rounded-md flex-1 shadow"
+      onSubmit= {handleSearch}
+      >
         <input 
         type="text" 
         placeholder="Search..." 
+        name="search-text"
         className="bg-transparent outline-none flex-1"
       />
         <button className="cursor-pointer">
