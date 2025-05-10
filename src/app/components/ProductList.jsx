327af0b175590f0772fe548e39d5c0ffa4aecf3d
Kadnan <ayeshoali@gmail.com>
2025-05-11 01:04:57 +0500

    feat: added categories on homepage

--- src/app/components/ProductList.jsx
+++ src/app/components/ProductList.jsx
@@ -36,6 +36,97 @@ const ProductList = () => {
           </button>
         </Link>
 
+        <Link
+          href={"/test"}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+        >
+          <div className="relative w-full h-80">
+            <Image
+              src="https://images.pexels.com/photos/6724313/pexels-photo-6724313.jpeg?auto=compress&cs=tinysrgb&w=1200"
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+            />
+            <Image
+              src="https://images.pexels.com/photos/6724313/pexels-photo-6724313.jpeg?auto=compress&cs=tinysrgb&w=1200"
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md"
+            />
+          </div>
+          <div className="flex justify-between">
+            <span className="font-medium">Product name</span>
+            <span className="font-semibold">$24</span>
+          </div>
+          <div className="text-sm text-gray-500"> Product description </div>
+
+          <button className="rounded-2xl ring-1 ring-redish text-redish w-max py-2 px-4 text-xs hover:bg-redish hover:text-white">
+            Add to Cart
+          </button>
+        </Link>
+        <Link
+          href={"/test"}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+        >
+          <div className="relative w-full h-80">
+            <Image
+              src="https://images.pexels.com/photos/6724313/pexels-photo-6724313.jpeg?auto=compress&cs=tinysrgb&w=1200"
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+            />
+            <Image
+              src="https://images.pexels.com/photos/6724313/pexels-photo-6724313.jpeg?auto=compress&cs=tinysrgb&w=1200"
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md"
+            />
+          </div>
+          <div className="flex justify-between">
+            <span className="font-medium">Product name</span>
+            <span className="font-semibold">$24</span>
+          </div>
+          <div className="text-sm text-gray-500"> Product description </div>
+
+          <button className="rounded-2xl ring-1 ring-redish text-redish w-max py-2 px-4 text-xs hover:bg-redish hover:text-white">
+            Add to Cart
+          </button>
+        </Link>
+        <Link
+          href={"/test"}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+        >
+          <div className="relative w-full h-80">
+            <Image
+              src="https://images.pexels.com/photos/6724313/pexels-photo-6724313.jpeg?auto=compress&cs=tinysrgb&w=1200"
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+            />
+            <Image
+              src="https://images.pexels.com/photos/6724313/pexels-photo-6724313.jpeg?auto=compress&cs=tinysrgb&w=1200"
+              alt=""
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md"
+            />
+          </div>
+          <div className="flex justify-between">
+            <span className="font-medium">Product name</span>
+            <span className="font-semibold">$24</span>
+          </div>
+          <div className="text-sm text-gray-500"> Product description </div>
+
+          <button className="rounded-2xl ring-1 ring-redish text-redish w-max py-2 px-4 text-xs hover:bg-redish hover:text-white">
+            Add to Cart
+          </button>
+        </Link>
+
     </div>
   );
 };
